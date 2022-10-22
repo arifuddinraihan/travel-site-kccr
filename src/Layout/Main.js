a20b5c32950ac9bc8d7043430cc5c224e010d9fa
arifuddinraihan <mdarifuddinraihan@gmail.com>
2022-10-22 17:49:55 +0600

    added login register forms

--- src/Layout/Main.js
+++ src/Layout/Main.js
@@ -7,7 +7,9 @@ const Main = () => {
     return (
         <div>
             <Navbar></Navbar>
-            <Outlet></Outlet>
+            <div>
+                <Outlet></Outlet>
+            </div>
             <Footer></Footer>
         </div>
     );
